64b07f98c7c1ebd1a60447c92aadf5430fa3cfe7
Lavorary <hei.lavorary@gmail.com>
2026-02-19 08:28:05 +0300

    feat : create a new table tax_config in the database

--- src/main/java/sql/schema.sql
+++ src/main/java/sql/schema.sql
@@ -31,3 +31,11 @@ INSERT INTO invoice_line (invoice_id, label, quantity, unit_price) VALUES
                                                                        (2, 'Produit A', 5, 100),
                                                                        (2, 'Produit C', 1, 200),
                                                                        (3, 'Produit B', 3, 50);
+
+CREATE TABLE tax_config (
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(250) NOT NULL,
+    rate NUMERIC(5,2) NOT NULL
+);
+
+
